2d2b9ae3f060a2a8fe32130107abeda83c27211c
mtimani <timani@etud.insa-toulouse.fr>
2018-10-03 14:17:56 +0200

    Changement des fichiers en php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Semèstre à l'étranger</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+    <h1>Semèstre à l'étranger</h1>
+</body>
+
+</html>
