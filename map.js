d7ce66b01490881c4bb50e94fe052d9c770d00da
mtimani <timani@etud.insa-toulouse.fr>
2018-10-06 11:05:31 +0200

    Quelques modifs

--- map.js
+++ map.js
@@ -1,10 +1,12 @@
 const NAV_MAP = {
-    187: { act: 'zoom', dir:  1, name: 'in' },
-    189: { act: 'zoom', dir: -1, name: 'out'},
-    37:  { act: 'move', dir: -1, name: 'left', axis: 0 },
-    38:  { act: 'move', dir: -1, name: 'up', axis: 1 },
-    39:  { act: 'move', dir:  1, name: 'right', axis: 0 },
-    40:  { act: 'move', dir:  1, name: 'down', axis: 1 }
+        187: { dir:  1, act: 'zoom', name: 'in' } /* + */,
+        61: { dir:  1, act: 'zoom', name: 'in' } /* + WTF, FF? */,
+        189: { dir: -1, act: 'zoom', name: 'out' } /* - */,
+        173: { dir: -1, act: 'zoom', name: 'out' } /* - WTF, FF? */,
+        37: { dir: -1, act: 'move', name: 'left', axis: 0 } /* ⇦ */,
+        38: { dir: -1, act: 'move', name: 'up', axis: 1 } /* ⇧ */,
+        39: { dir:  1, act: 'move', name: 'right', axis: 0 } /* ⇨ */,
+        40: { dir:  1, act: 'move', name: 'down', axis: 1 } /* ⇩ */
     },
     _SVG = document.querySelector('svg'),
     VB = _SVG.getAttribute('viewBox').split(' ').map(c => +c),
@@ -16,12 +18,12 @@ let nav = null, tg = Array(4), f = 0, rID = null;
 function update(){
     let k = ++f/NF, j = 1 - k, cvb = VB.slice();
 
-    if(nav.act == 'zoom'){
+    if(nav.act === 'zoom'){
         for(let i = 0; i < 4; i++){
             cvb[i] = j*VB[i] + k*tg[i]
         }
     }
-    else if(nav.act == 'move') {
+    else if(nav.act === 'move') {
         cvb[nav.axis] = j*VB[nav.axis] + k*tg[nav.axis]
     }
 
@@ -39,13 +41,16 @@ function update(){
     rID = requestAnimationFrame(update)
 }
 
+addEventListener('keydown', e => { e.preventDefault() }, false);
+addEventListener('keypress', e => { e.preventDefault() }, false);
+
 addEventListener('keyup',e => {
     if(e.keyCode in NAV_MAP) {
         nav = NAV_MAP[e.keyCode];
 
-        if(nav.act == 'zoom'){
-            if ((nav.dir == -1 && VB[2] >= DMAX[0]) ||
-                (nav.dir == 1 && VB[2] <= WMIN)){
+        if(nav.act === 'zoom'){
+            if ((nav.dir === -1 && VB[2] >= DMAX[0]) ||
+                (nav.dir === 1 && VB[2] <= WMIN)){
                 console.log('cannot zoom ${nav.name} more');
             }
             for(let i = 0; i < 2 ; i++) {
@@ -53,9 +58,9 @@ addEventListener('keyup',e => {
                 tg[i] = .5 * (DMAX[i] - tg[i + 2])
             }
         }
-        else if(nav.act == 'move'){
-            if ((nav.dir == -1 && VB[nav.axis] <= 0) ||
-                (nav.dir == 1 && VB[nav.axis] >= DMAX[nav.axis] - VB[nav.axis] + 2)){
+        else if(nav.act === 'move'){
+            if ((nav.dir === -1 && VB[nav.axis] <= 0) ||
+                (nav.dir === 1 && VB[nav.axis] >= DMAX[nav.axis] - VB[nav.axis] + 2)){
                 console.log('cannot move ${nav.name} more');
                 return
             }
@@ -66,3 +71,4 @@ addEventListener('keyup',e => {
 
     }
 },false);
+
