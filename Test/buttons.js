e707e26cfda2721f4161e29d8b1410e5f0877263
mtimani <timani@etud.insa-toulouse.fr>
2018-10-18 23:09:46 +0200

    Buttons PO

---
+++ Test/buttons.js
@@ -0,0 +1,15 @@
+document.getElementById("MIC").addEventListener("click", function(){
+
+});
+
+document.getElementById("IMACS").addEventListener("click", function(){
+
+});
+
+document.getElementById("ICBE").addEventListener("click", function(){
+
+});
+
+document.getElementById("IC").addEventListener("click", function(){
+
+});
