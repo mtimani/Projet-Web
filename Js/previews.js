69ae7311be1f86be8f15fb4c3b54e7ba2157b973
AbdelSaTd <tarnagda@etud.insa-toulouse.fr>
2018-11-30 21:01:05 +0100

    merge processed

--- Js/previews.js
+++ Js/previews.js
@@ -5,24 +5,56 @@
 
 
 var c_BF = $('#c_BF');
+//var prev = $('#preview_BF');
 
 c_BF.mouseenter(function (e){
+    clearTimeout(c_BF);
     c_BF=setTimeout(function(){
+        var posY = e.pageY-300; //300 is 3/4 of the preview's height
         console.log("Hello !");
-        $('#preview_BF').css({ "visibility": "visible", "left": e.pageX, "right": e.pageX, "background": "white"});
-    }, 2000);
+        $('#preview_BF').css({ "visibility": "visible", "left": e.pageX, "top": posY});
+    }, 500);
 
+
+});
+
+c_BF.click(function (e){
+    var posY = e.pageY-300;
+    console.log("Hello !");
+    $('#preview_BF').css({ "visibility": "visible", "left": e.pageX, "top": posY});
 });
 
 
 
-
-c_BF.mouseout(function (e){
-
-    console.log("Olleh !");
+c_BF.mouseout(function (){
     clearTimeout(c_BF);
 
+    c_BF=setTimeout(function(){
+        $('#preview_BF').css({ "visibility": "hidden"});
+        console.log("Olleh !");
+    }, 500);
+
+    $('#preview_BF').mouseenter(function (e){
+        clearTimeout(c_BF);
+        $('#preview_BF').css({ "visibility": "visible"});
+        console.log("Hello HOLY WORK !");
+    });
 });
+
+
+
+
+c_BF.mouseout(function (){
+    clearTimeout(c_BF);
+    c_BF=setTimeout(function(){
+        $('#preview_BF').css({ "visibility": "hidden"});
+        console.log("Olleh !");
+    }, 500);
+
+});
+
+
+
 
 
 
