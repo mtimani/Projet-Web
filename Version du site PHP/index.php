f34dff6d06548bce56cce7219bfd2f7aabdf37c3
mtimani <timani@etud.insa-toulouse.fr>
2018-12-14 20:31:57 +0100

    Homepage title

--- Version du site PHP/index.php
+++ Version du site PHP/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
+        <title>Accueil</title>
         <link rel="stylesheet" href="Css/menu.css"/>
         <link rel="stylesheet" href="Css/previews.css"/>
         <link rel="stylesheet" href="Css/app.css">
