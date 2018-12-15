e13b84cb2d5f73f3092f5abbd5efb9ec09cfa21c
mtimani <timani@etud.insa-toulouse.fr>
2018-12-15 10:43:06 +0100

    Ajout de commentaires

--- Js/menu.js
+++ Js/menu.js
@@ -1,4 +1,4 @@
-function responsiveFunction() {
+function responsiveFunction() { // Fonction qui ajoute la classe "responsif" ou l'enlève si elle a déjà été rajoutée
     var x = document.getElementById("menu");
     if (x.className === "barrenav") {
         x.className += " responsif";
